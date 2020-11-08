dd2bfa74d6376ff2dcb9a984ddb476a8e3985431
krecke <krecke@gmail.com>
2020-11-08 14:46:50 -0300

    New highlights at 'at a glance' section, new header and text improvements

--- smartguestbook/js/script.js
+++ smartguestbook/js/script.js
@@ -1,28 +1,26 @@
-window.addEventListener('scroll', () => {
-	if(pageYOffset > 30){
-		const menu = document.querySelector('.menu');
-		menu.style.boxShadow = "0 0 20px rgb(0,0,0, 0.1)";
-		menu.style.transition = "1s";
-	}
-});
+// window.addEventListener('scroll', () => {
+// 	if(pageYOffset > 30){
+// 		const menu = document.querySelector('.menu');
+// 		menu.style.boxShadow = "0 0 20px rgb(0,0,0, 0.1)";
+// 		menu.style.transition = "1s";
+// 	}
+// });
 
-const galleryimg = document.querySelectorAll('.pictures img');
-const fullgallery = document.querySelector('.fullgallery');
+// const galleryimg = document.querySelectorAll('.pictures img');
+// const fullgallery = document.querySelector('.fullgallery');
 
-console.log(fullgallery);
 
-for(let i=0; i < galleryimg.length; i++){
+// for(let i=0; i < galleryimg.length; i++){
 		
-		let img = galleryimg[i];
-		let imgsrc = img.src.split("/").pop();
-		let imgname = imgsrc.split(".").shift();
+// 		let img = galleryimg[i];
+// 		let imgsrc = img.src.split("/").pop();
+// 		let imgname = imgsrc.split(".").shift();
 
-		img.addEventListener('click', () => {
-			//fullgallery.src = `https://carebnb.app/smartguestbook/img/gallery/${imgname}.jpg`;
-			// const scrollposition = document.body.scrollTop;
-			// console.log(scrollposition);
-			//fullgallery.style.top = scrollposition;
-		});
+// 		img.addEventListener('click', () => {
+// 			//fullgallery.src = `https://carebnb.app/smartguestbook/img/gallery/${imgname}.jpg`;
+// 			// const scrollposition = document.body.scrollTop;
+// 			// console.log(scrollposition);
+// 			//fullgallery.style.top = scrollposition;
+// 		});
 	
-	}
-
+// 	}
